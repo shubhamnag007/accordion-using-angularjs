a11d0346b0dc4bdaf470f43e534c0d693f9b6b8a
shubham <shubhamnag007@gmail.com>
2021-12-02 13:19:22 +0530

    vscode://vscode.github-authentication/did-authenticate?windowid=1&code=2d707f171149e380e90c&state=c685dc7a-2add-4057-877c-88dc4a69d8cd

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatExpansionModule} from '@angular/material/expansion';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatExpansionModule,
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
